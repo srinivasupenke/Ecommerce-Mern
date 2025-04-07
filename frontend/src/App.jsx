1e66a30835934cb98e30ea5f9fca579b54eceb0b
srinivasupenke <srinudps123@gmail.com>
2025-04-07 15:56:12 +0530

    Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+import Orders from "./pages/Order";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="px-4 sm:px-[7vw] md:px[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
